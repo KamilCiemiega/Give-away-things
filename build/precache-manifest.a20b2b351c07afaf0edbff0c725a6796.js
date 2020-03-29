c8c33ec5f526173a76e125cfc0e8bf069bc564be
Kamil Ciemiega <kamil13241012@interia.pl>
2020-03-29 23:17:59 +0200

    changes

---
+++ build/precache-manifest.a20b2b351c07afaf0edbff0c725a6796.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3e5f6b9a973a8c009774584ea8dec4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e79abbeb8244bea66a8",
+    "url": "/static/css/main.f0cb9639.chunk.css"
+  },
+  {
+    "revision": "706a4bc3d46cbc598b61",
+    "url": "/static/js/2.a2edaba4.chunk.js"
+  },
+  {
+    "revision": "95803d0de74e85e9fcd1b51dbbc0df72",
+    "url": "/static/js/2.a2edaba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e79abbeb8244bea66a8",
+    "url": "/static/js/main.16103463.chunk.js"
+  },
+  {
+    "revision": "71d9a9b4e2eef3e1a1b0",
+    "url": "/static/js/runtime-main.55b38259.js"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "6baf5950179f12f109e487a7ec03faac",
+    "url": "/static/media/Decoration.6baf5950.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/static/media/Form-Hero-Image.3bee335a.jpg"
+  },
+  {
+    "revision": "ef2519d20c70fe42a96e8c744772c6ef",
+    "url": "/static/media/Hero.ef2519d2.png"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "766879f461e6740c09be6ef64cb6618b",
+    "url": "/static/media/arrow-down.766879f4.svg"
+  },
+  {
+    "revision": "7db36abd712c228ea96e6206440a9eb3",
+    "url": "/static/media/columns.7db36abd.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/static/media/contact-form.f4db7ddd.jpg"
+  }
+]);
